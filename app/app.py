e3d572db02436f5d6db21a78d209cc989a966313
Satishkona-7 <konasatish4444@gmail.com>
2026-05-24 16:30:48 +0530

    Initial Python DevSecOps project

---
+++ app/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return jsonify({
+        "message": "Python DevSecOps Demo",
+        "status": "success"
+    })
+
+
+@app.route('/health')
+def health():
+    return jsonify({
+        "status": "UP"
+    })
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
